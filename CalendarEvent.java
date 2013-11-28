1c7b4a6e0098e14bf0a013391ee0c8681f5d85ef
ryan <ryan@M1530.(none)>
2013-11-28 04:37:51 -0800

    Implemented CalenderEvent.java

--- CalendarEvent.java
+++ CalendarEvent.java
@@ -1,40 +1,87 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * This class stores information about an event on the calendar. Part of the model.
- * @author
+ * @author Ryan Vo
+ * @version 2013-11-28
  *
  */
 public class CalendarEvent 
 {
-	String getEventName()
+	/**
+	 * Constructs a CalendarEvent object. The event shall start and end on the same day.
+	 * @param eventName The name of the event.
+	 * @param year The year the event will take place.
+	 * @param month The month the event will take place (1-12).
+	 * @param day The day the event will take place (1-31).
+	 * @param startHour The hour the event will take place (0-23).
+	 * @param endHour The hour the event will end (0-23).
+	 */
+	public CalendarEvent(String eventName, int year, int month, int day, 
+			int startHour, int endHour)
 	{
+		this.eventName = eventName;
+		this.startDate = new GregorianCalendar(year, month - 1, day, startHour, 0);
+		this.endDate = new GregorianCalendar(year, month - 1, day, endHour, 0);
+	}
+	
+	/**
+	 * Gets the name of the event.
+	 * @return The name of the event.
+	 */
+	public String getEventName()
+	{
+		return eventName;
+	}
+	
+	/**
+	 * Gets the year the event will take place.
+	 * @return The year.
+	 */
+	public int getYear()
+	{
+		return startDate.get(Calendar.YEAR);
+	}
+	
+	/**
+	 * Gets the month the event will take place.
+	 * @return The month.
+	 */
+	public int getMonth()
+	{
+		return startDate.get(Calendar.MONTH) + 1; // Month uses zero-index.
+	}
 
+	/**
+	 * Gets the day the event will take place.
+	 * @return The day.
+	 */
+	public int getDay()
+	{
+		return startDate.get(Calendar.DATE);
 	}
 	
-	int getYear()
+	/**
+	 * Gets the hour the event will start at.
+	 * @return The hour the event will start at.
+	 */
+	public int getStartHour()
 	{
-		
+		return startDate.get(Calendar.HOUR_OF_DAY);
 	}
 	
-	int getMonth()
+	/**
+	 * Gets the hour the event will end at.
+	 * @return The hour the event will end at.
+	 */
+	public int getEndHour()
 	{
-		
-	}
-
-	int getDay()
-	{
-		
-	}
-	
-	int getStartHour()
-	{
-		
-	}
-	
-	int getEndHour()
-	{
-		
+		return endDate.get(Calendar.HOUR_OF_DAY);
 	}
 	
 	// Should the assumption be made that events start and end on the same day?
-	// Can store time in a GregorianCalendar.
+	private String eventName;
+	private GregorianCalendar startDate;
+	private GregorianCalendar endDate;
 }
