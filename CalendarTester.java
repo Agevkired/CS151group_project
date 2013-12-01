4fd801944ab614c72a0051675a3a5a922f2f8cf4
Agevkired <derik_vega@yahoo.com>
2013-12-01 04:58:27 -0800

    Create CalendarTester.java

---
+++ CalendarTester.java
@@ -0,0 +1,22 @@
+package cs151_project;
+
+/**
+ * This class runs the entire project. Everything comes together here.
+ * @author Cobra
+ *
+ */
+public class CalendarTester 
+{
+	public static void main(String Args[])
+	{
+		System.out.println("Hello, world!");
+		/*
+		CalendarController maincalendar = new maincalendar;
+		
+		maincalendar.setSize(500,300);
+		maincalendar.setTitle("Calendar");
+		maincalendar.setDefaultCloseOperation(3);
+		maincalendar.setVisible(true);
+		*/
+	}
+}
